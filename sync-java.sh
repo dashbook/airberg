a189e2cb8bc4fe43cfcc372beb185708a8acf86d
Jan Kaul <jankaul@mailbox.org>
2024-07-08 11:30:18 +0200

    use tmp for config

--- sync-java.sh
+++ sync-java.sh
@@ -1,4 +1,4 @@
-bin/$AIRBYTE_SOURCE --config config/source.json --discover | grep "^{\"type\":\"CATALOG\".*}" > discover.json
-destination-iceberg --config config/destination.json --catalog discover.json > catalog.json
-destination-iceberg --config config/destination.json --catalog discover.json --state > state.json
-bin/$AIRBYTE_SOURCE --config config/source.json --catalog catalog.json --read | destination-iceberg --config config/destination.json --catalog catalog.json --write
+bin/$AIRBYTE_SOURCE --config /tmp/config/source.json --discover | grep "^{\"type\":\"CATALOG\".*}" > discover.json
+destination-iceberg --config /tmp/config/destination.json --catalog discover.json > catalog.json
+destination-iceberg --config /tmp/config/destination.json --catalog discover.json --state > state.json
+bin/$AIRBYTE_SOURCE --config /tmp/config/source.json --catalog catalog.json --read | destination-iceberg --config /tmp/config/destination.json --catalog catalog.json --write
