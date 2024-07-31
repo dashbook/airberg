60a2928a094036fd89fbece0ebf0b3db96b3bd86
Jan Kaul <jankaul@mailbox.org>
2024-07-31 19:04:21 +0200

    update entrypoint

--- sync-java.sh
+++ sync-java.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+echo "Discovering"
 bin/$AIRBYTE_SOURCE --config /tmp/config/source.json --discover | grep "^{\"type\":\"CATALOG\".*}" > discover.json
+
+echo "Configuring catalog"
 destination-iceberg --config /tmp/config/destination.json --catalog discover.json > catalog.json
+
+echo "Getting state"
 destination-iceberg --config /tmp/config/destination.json --catalog discover.json --state > state.json
+
+echo "Syncing"
 bin/$AIRBYTE_SOURCE --config /tmp/config/source.json --catalog catalog.json --read | destination-iceberg --config /tmp/config/destination.json --catalog catalog.json --write
