2960e42ed0ddf1ce8cf147068a5f70463dd4de4b
Jan Kaul <jankaul@mailbox.org>
2024-10-09 15:53:22 +0200

    fix python script execution

--- airbyte-integrations/connectors/source-s3/sync-python.sh
+++ airbyte-integrations/connectors/source-s3/sync-python.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Discovering"
-python main.py --config /tmp/config/source.json --discover | grep "^{\"type\":\"CATALOG\".*}" > discover.json
+python main.py discover --config /tmp/config/source.json | grep "^{\"type\":\"CATALOG\".*}" > discover.json
 
 echo "Configuring catalog"
 destination-iceberg --config /tmp/config/destination.json --catalog discover.json > catalog.json
@@ -10,4 +10,4 @@ echo "Getting state"
 destination-iceberg --config /tmp/config/destination.json --catalog discover.json --state > state.json
 
 echo "Syncing"
-python main.py --config /tmp/config/source.json --catalog catalog.json --state state.json --read | destination-iceberg --config /tmp/config/destination.json --catalog catalog.json --write
+python main.py read --config /tmp/config/source.json --catalog catalog.json --state state.json | destination-iceberg --config /tmp/config/destination.json --catalog catalog.json --write
