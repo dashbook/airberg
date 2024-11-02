e97dea2196d2dd39fa2eee7e33ff1b1ffed43d31
Jan Kaul <jankaul@mailbox.org>
2024-11-02 09:40:33 +0100

    update sync script

--- sync-java.sh
+++ sync-java.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-cat /tmp/config/source.json | envsubst > /tmp/config/temp.json && mv /tmp/config/temp.json /tmp/config/source.json
-cat /tmp/config/destination.json | envsubst > /tmp/config/temp.json && mv /tmp/config/temp.json /tmp/config/destination.json
+cat /tmp/config/source.json | envsubst > /airbyte/source.json
+cat /tmp/config/destination.json | envsubst > /airbyte/destination.json
 
 echo "Discovering"
-bin/$AIRBYTE_SOURCE --config /tmp/config/source.json --discover | grep "^{\"type\":\"CATALOG\".*}" > discover.json
+bin/$AIRBYTE_SOURCE --config source.json --discover | grep "^{\"type\":\"CATALOG\".*}" > discover.json
 
 echo "Configuring catalog"
-destination-iceberg --config /tmp/config/destination.json --catalog discover.json > catalog.json
+destination-iceberg --config destination.json --catalog discover.json > catalog.json
 
 echo "Getting state"
-destination-iceberg --config /tmp/config/destination.json --catalog discover.json --state > state.json
+destination-iceberg --config destination.json --catalog discover.json --state > state.json
 
 echo "Syncing"
-bin/$AIRBYTE_SOURCE --config /tmp/config/source.json --catalog catalog.json --state state.json --read | destination-iceberg --config /tmp/config/destination.json --catalog catalog.json --write
+bin/$AIRBYTE_SOURCE --config source.json --catalog catalog.json --state state.json --read | destination-iceberg --config destination.json --catalog catalog.json --write
