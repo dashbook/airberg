5cecab355b417eba15354f1811de98b5832f9d34
Jan Kaul <jankaul@mailbox.org>
2024-11-28 19:52:11 +0100

    add faker source

---
+++ airbyte-integrations/connectors/source-faker/sync-python.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat /tmp/config/source.json | envsubst > /airbyte/source.json 
+cat /tmp/config/destination.json | envsubst > /airbyte/destination.json
+
+echo "Discovering"
+python main.py discover --config source.json | grep "^{\"type\": \"CATALOG\".*}" > discover.json
+
+echo "Configuring catalog"
+destination-iceberg --config destination.json --catalog discover.json > catalog.json
+
+echo "Getting state"
+destination-iceberg --config destination.json --catalog discover.json --state > state.json
+
+echo "Syncing"
+python main.py read --config source.json --catalog catalog.json --state state.json | destination-iceberg --config destination.json --catalog catalog.json --write
