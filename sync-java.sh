2de803f187194a0d7464d550fc03341cb0a54f06
Jan Kaul <jankaul@mailbox.org>
2025-11-12 14:22:55 +0100

    use catalog.json for state

--- sync-java.sh
+++ sync-java.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-cat /tmp/config/source.json | envsubst > /airbyte/source.json
-cat /tmp/config/destination.json | envsubst > /airbyte/destination.json
+cat /tmp/config/source.json | envsubst >/airbyte/source.json
+cat /tmp/config/destination.json | envsubst >/airbyte/destination.json
 
 echo "Discovering"
-bin/$AIRBYTE_SOURCE --config source.json --discover | grep "^{\"type\":\"CATALOG\".*}" > discover.json
+bin/$AIRBYTE_SOURCE --config source.json --discover | grep "^{\"type\":\"CATALOG\".*}" >discover.json
 
 echo "Configuring catalog"
-destination-iceberg --config destination.json --catalog discover.json > catalog.json
+destination-iceberg --config destination.json --catalog discover.json >catalog.json
 
 echo "Getting state"
-destination-iceberg --config destination.json --catalog discover.json --state > state.json
+destination-iceberg --config destination.json --catalog catalog.json --state >state.json
 
 echo "Syncing"
 bin/$AIRBYTE_SOURCE --config source.json --catalog catalog.json --state state.json --read | destination-iceberg --config destination.json --catalog catalog.json --write
